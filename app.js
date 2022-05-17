33a0904191b9cee7e144a0c11cd996b54221e2ad
Eyad Ahmed <eyad.ahmed.abdelfatah@gmail.com>
2022-05-17 04:04:09 +0200

    new update

--- app.js
+++ app.js
@@ -19,4 +19,6 @@ app.use((req, res, next) => {
     res.status(404).render('404');
 });
 
-app.listen(8000);
+app.listen( process.env.PORT || 8000, function () {
+    console.log( "Server is running on port 8000" );
+} );
