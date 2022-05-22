0ad50af1d4d2702db92c04d438248f4d54b0170a
Eyad Ahmed <eyad.ahmed.abdelfatah@gmail.com>
2022-05-22 22:10:03 +0200

    connect.js

--- public/util/connect.js
+++ public/util/connect.js
@@ -24,18 +24,14 @@ connection.on( 'connect', function ( err ) {
 connection.connect();
 
 var Request = require( 'tedious' ).Request
-var TYPES = require( 'tedious' ).TYPES;
+
 
 function executeStatement1() {
-    request = new Request( "INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function ( err ) {
+    request = new Request( "SELECT TOP (1000) * FROM [dbo].[Category]", function ( err ) {
         if ( err ) {
             console.log( err );
         }
     } );
-    request.addParameter( 'Name', TYPES.NVarChar, 'SQL Server Express 2014' );
-    request.addParameter( 'Number', TYPES.NVarChar, 'SQLEXPRESS2014' );
-    request.addParameter( 'Cost', TYPES.Int, 11 );
-    request.addParameter( 'Price', TYPES.Int, 11 );
     request.on( 'row', function ( columns ) {
         columns.forEach( function ( column ) {
             if ( column.value === null ) {
