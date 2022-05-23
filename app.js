a97721f2bfb6bd7325be598d62883d63ed05bf18
Abdelwahab <hopakamal25@gmail.com>
2022-05-23 02:23:03 +0200

    connect contactUs to database

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use((req, res, next) => {
     res.status(404).render('404');
 });
 
+
+
 app.listen( process.env.PORT || 8000, function () {
     console.log( "Server is running on port 8000" );
 } );
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,36 @@
-const bodyParser = require( 'body-parser' )
+const bodyParser = require( 'body-parser' );
+const { request } = require('express');
+
+var Connection = require( 'tedious' ).Connection;
+var config = {
+    server: 'linkeditem.database.windows.net', //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'admin-Eyad', //update me
+            password: 'Linked-item' //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'linkeditem' //update me
+    }
+};
+var connection = new Connection( config );
+connection.on( 'connect', function ( err ) {
+    // If no error, then good to proceed.  
+    console.log( "Connected" );
+
+} );
+connection.connect();
+
+var Request = require( 'tedious' ).Request
+
+
 
 const express = require('express');
+
 
 const app = express();
 app.use( express.static( "public" ) );
@@ -37,13 +67,32 @@ app.get('/', (req, res ) => {
 app.post( '/CreateAccount', ( req, res ) => {
     console.log( req.body.Username );
 } );
-module.exports = {
-    'routes': app
-};
 
-app.post( '/ContactUs', ( req, res ) => {
-    console.log( req.body.phone );
-} );
+
+app.post('/ContactUs',function(req,res) {
+    var a=req.body.name;
+    var b=req.body.phone;
+    var c=req.body.email;
+    var d=req.body.message;
+   var request = new Request( "insert into contact_us(contact_name,contact_phone_no,contact_email,contact_message)values('"+a+"','"+b+"','"+c+"','"+d+"')", function ( err ) {
+        if ( err ) {
+            console.log( err );
+        }
+        console.log("1 record inserted");
+
+    });
+
+    // Close the connection after the final event emitted by the request, after the callback passes
+    request.on( "requestCompleted", function ( rowCount, more ) {
+        connection.close();
+    } );
+    connection.execSql( request );
+    return res.redirect('/ContactUs');
+    
+
+});
+
+
 module.exports = {
     'routes': app
 };
