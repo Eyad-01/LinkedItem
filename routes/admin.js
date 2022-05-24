c515a120a0564fd2381bc2bf970588ebc691b832
Eyad Ahmed <eyad.ahmed.abdelfatah@gmail.com>
2022-05-25 00:40:29 +0200

    all updates

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,33 @@
-const bodyParser = require( 'body-parser' )
+const bodyParser = require( 'body-parser' );
+const { request } = require('express');
+
+var Connection = require( 'tedious' ).Connection;
+var config = {
+    server: 'linkeditem.database.windows.net', //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'admin-Eyad', //update me
+            password: 'Linked-item' //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'linkeditem' //update me
+    }
+};
+var connection = new Connection( config );
+connection.on( 'connect', function ( err ) {
+    // If no error, then good to proceed.
+    console.log( "Connected" );
+
+} );
+connection.connect();
+
+var Request = require( 'tedious' ).Request;
+
+
 
 const express = require( 'express' );
 
@@ -24,6 +53,28 @@ app.get('/manage-product', (req, res) => {
 
 app.post('/manage-product', (req, res) => {
     res.render('manage-products');
+});
+
+app.get('/manage-company', (req, res) => {
+    res.render('manage-company');
+});
+
+
+/* Add Student */
+app.post('/addCompany', function (req, res) {
+        var request= new Request("INSERT INTO Company(Company_id,Company_type,Company_name,company_address,company_phone_no,company_email) VALUES('"+1+"','" + req.body.type + "', '" + req.body.name + "', '" + req.body.address +"','" +req.body.phone +"', '" + req.body.email + "')", function ( err ) {
+        if ( err ) {
+            console.log( err );
+        }
+        console.log("1 record inserted");
+    });
+
+        request.on( "requestCompleted", function ( rowCount, more ) {
+            connection.close();
+        } );
+        connection.execSql( request );
+        return res.redirect('/admin/manage-company');
+
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,33 @@
-const bodyParser = require( 'body-parser' )
+const bodyParser = require( 'body-parser' );
+const { request } = require('express');
+
+var Connection = require( 'tedious' ).Connection;
+var config = {
+    server: 'linkeditem.database.windows.net', //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'admin-Eyad', //update me
+            password: 'Linked-item' //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'linkeditem' //update me
+    }
+};
+var connection = new Connection( config );
+connection.on( 'connect', function ( err ) {
+    // If no error, then good to proceed.
+    console.log( "Connected" );
+
+} );
+connection.connect();
+
+var Request = require( 'tedious' ).Request
+
+
 
 const express = require('express');
 
@@ -34,9 +63,59 @@ app.get('/', (req, res ) => {
     res.render('index');
 } );
 
-app.post( '/CreateAccount', ( req, res ) => {
-    console.log( req.body.Username );
+
+
+app.post('/ContactUs',function(req,res) {
+    var a=req.body.name;
+    var b=req.body.phone;
+    var c=req.body.email;
+    var d=req.body.message;
+   var request = new Request( "insert into contact_us(contact_name,contact_phone_no,contact_email,contact_message)values('"+a+"','"+b+"','"+c+"','"+d+"')", function ( err ) {
+        if ( err ) {
+            console.log( err );
+        }
+        console.log("1 record inserted");
+
+    });
+
+    // Close the connection after the final event emitted by the request, after the callback passes
+    request.on( "requestCompleted", function ( rowCount, more ) {
+        connection.close();
 } );
+    connection.execSql( request );
+    return res.redirect('/ContactUs');
+
+
+});
+
+
+
+app.post('/CreateAccount',function(req,res) {
+    var a=req.body.fname;
+    var b=req.body.sname;
+    var c=req.body.phone;
+    var d=req.body.email;
+    var e=req.body.password;
+    var f=0;
+   var request = new Request( "insert into users(user_first_name,user_last_name,user_phone_no,user_email,user_password,user_type)values('"+a+"','"+b+"','"+c+"','"+d+"','"+e+"','"+f+"')", function ( err ) {
+        if ( err ) {
+            console.log( err );
+        }
+        console.log("1 record inserted");
+
+    });
+
+    // Close the connection after the final event emitted by the request, after the callback passes
+    request.on( "requestCompleted", function ( rowCount, more ) {
+        connection.close();
+    } );
+    connection.execSql( request );
+    return res.redirect('/SignIn');
+
+
+});
+
+
 module.exports = {
     'routes': app
 };
